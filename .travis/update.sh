09af1d98139d8834bf82b90d55cadf52a1f6238f
Yu Jing <yujing5b5d@gmail.com>
2018-05-04 16:31:06 -0400

    Auto Commit: 2018-05-04 16:31:06 EDT

--- .travis/update.sh
+++ .travis/update.sh
@@ -5,8 +5,12 @@ if [[ $(git status --porcelain) ]]; then
   # Changes
   echo "Changed: make an auto commit"
   git add .
-  git commit -m "Auto Commit: $(date '+%Y-%m-%d %H:%M:%S')"
-  git push https://${GH_TOKEN}@github.com/argcv/go-argcvapis.git
+  git commit -m "Auto Commit: $(date '+%Y-%m-%d %H:%M:%S %Z')"
+  TMP_BRANCH="tmp$(date '+%Y-%m-%d-%H-%M-%S-%Z')"
+  git branch $TMP_BRANCH
+  git checkout master
+  git merge $TMP_BRANCH
+  git push https://${GH_TOKEN}@github.com/argcv/go-argcvapis.git master
   echo "Updated..."
 else
   echo "Not changed: skip..."
