494c3259633c83b8fee346ad5404a408ba3586b4
Travis CI User <travis@example.org>
2018-05-30 20:31:18 +0000

    Auto Commit: 2018-05-30 20:31:18 UTC

--- longrunning/operations.pb.gw.go
+++ longrunning/operations.pb.gw.go
@@ -158,14 +158,14 @@ func RegisterOperationsHandlerFromEndpoint(ctx context.Context, mux *runtime.Ser
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Printf("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Printf("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
